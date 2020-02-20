31e311ed153ae03059f4585c1dcdda1020698e68
Klein <florian.klein@te.com>
2020-02-20 15:21:50 +0100

    fixed heuristics function

--- controller.py
+++ controller.py
@@ -53,12 +53,15 @@ class AIController(Controller):
 		max_value = -200
 		best_column = None
 
+		# check how many moves are left and adapt depth
+		depth = min(5, np.sum(board == 0)) 
+
 		for row, column in self.valid_moves(board, active_player):
 
 			new_board = np.copy(board)
 			new_board[row, column] = active_player
 
-			value = self.min_max(new_board, active_player, active_player*(-1), 4)
+			value = self.min_max(new_board, active_player, active_player*(-1), depth)
 			if value > max_value:
 				max_value = value
 				best_column = column
@@ -70,21 +73,27 @@ class AIController(Controller):
 
 	def min_max(self, board, player, active_player, depth):
 
+		# at maximum depth, return board evaluation
 		if not depth:
 			return self.evaluate_board(board, player)
 
 		# if at any time any parties win is imminent, return immediately
-		if min_max := self.check_win_condition(board, player):
-			return min_max
+		early_win = self.check_win_condition(board, player)
+		if early_win:
+			return early_win
 
+		# set comparison value in dependence of whose turn it is
 		min_max = 101 * (1,-1)[player == active_player]
 
+		# get all possible moves
 		for row, column in self.valid_moves(board,active_player):
 
+			# create new board with current move updated
 			new_board = np.copy(board)
 			new_board[row, column] = active_player
 
 			evaluation = self.min_max(new_board, player, active_player*(-1), depth-1)
+
 			if player == active_player:
 				min_max = max(evaluation, min_max)
 			else:
@@ -94,6 +103,7 @@ class AIController(Controller):
 
 
 	def valid_moves(self, board, active_player):
+
 		for column in range(7):
 			(column_values,) = np.where(board[:,column] == 0)
 			
@@ -128,13 +138,13 @@ class AIController(Controller):
 
 	def evaluate_board(self, board, player):
 
-		# on finished game return immediatly
+		# on finished game return immediately
 		evaluation = self.check_win_condition(board, player)
 
 		if evaluation:
 			return evaluation
 		
-		# check for threats, namely "XXX "
+		# check for threats, namely 3 out of 4 are one color, the 4th is empty
 		kernel = [
 			np.array([[1, 1, 1, 1]], dtype=np.int8),
 			np.array([[1], [1], [1], [1]], dtype=np.int8),
@@ -145,16 +155,14 @@ class AIController(Controller):
 		for k in kernel:
 
 			convolusion = convolve2d(board, k, mode="valid")
-			# Check for winning position
-			if np.any(convolusion == 3 * player):
-				evaluation += 5
-			# Check for losing position
-			elif np.any(convolusion == -3 * player):
-				evaluation -= 5
+			
+			if np.any(abs(convolusion) == 3):
+				evaluation += 5 * np.sum(convolusion == 3 * player)
+				evaluation -= 5 * np.sum(convolusion == -3 * player)
 
 		
 		# check how many own pieces are in the middle row
-		evaluation = 3 * np.sum(board[:,3] == player)
+		evaluation += 3 * np.sum(board[:,3] == player)
 
 		return evaluation
 
